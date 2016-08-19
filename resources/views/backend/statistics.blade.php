0656fdaf349e1cc9fc25d457157ef08bcc2ce5df
Nicholas Stone <Nicholas-Stone@outlook.com>
2016-08-19 12:06:58 +0800

    完成数据统计导出为Excel与数据打印功能

--- resources/views/backend/statistics.blade.php
+++ resources/views/backend/statistics.blade.php
@@ -17,8 +17,10 @@
         </div>
         <div class="box-body">
             <div id="jqxgrid"></div>
-            <br>
-            <button class="btn btn-default btn-flat" id="excel">导出为Excel</button>
+            <div class="btn-group">
+                <button class="btn btn-default btn-flat" id="excel">导出为Excel</button>
+                <button class="btn btn-default btn-flat" id="print">打印</button>
+            </div>
         </div>
     </div>
 @endsection
@@ -29,5 +31,21 @@
     <script>
         $(function(){$.ajax({url:"{{ route('admin.statistics.get') }}",data:{_token:"{{ csrf_token() }}"},method:"post",success:function(b){console.log(b);var a={localData:b,dataType:"json",dataFields:[{name:"id",type:"number"},{name:"type",type:"string"},{name:"verification",type:"bool"},{name:"fancy",type:"string"},{name:"created_at",type:"string"},{name:"updated_at",type:"string"},{name:"branch_name",type:"string",map:"branch>name"},{name:"branch_type",type:"string",map:"branch>type"},{name:"university",type:"string",map:"branch>university>name"},{name:"province",type:"string",map:"branch>university>province>name"}]};$("#jqxgrid").jqxGrid({width:"100%",theme:"bootstrap",source:new $.jqx.dataAdapter(a),filterable:true,sortable:true,pageable:true,autoshowfiltericon:true,localization:"de",columns:[{text:"#",datafield:"id"},{text:"类型",datafield:"type"},{text:"审核结果",datafield:"verification",cellsrenderer:function(e,c,d){return d?"<span style='background-color: darkgreen;position: absolute;top: 7px;left: 10px; border-radius: 3px; color: white;padding: 0 5px 0 5px'>通过</span>":"<span style='background-color: darkred;position: absolute;top: 7px;left: 10px; border-radius: 3px; color: white;padding: 0 5px 0 5px'>未通过</span>"}},{text:"支部类型",datafield:"branch_type"},{text:"点赞次数",datafield:"fancy"},{text:"提交时间",datafield:"created_at"},{text:"审核通过时间",datafield:"updated_at"},{text:"支部名称",datafield:"branch_name"},{text:"所在学校",datafield:"university"},{text:"所在地区",datafield:"province"}]})}})});
         $("#excel").click(function () {$("#jqxgrid").jqxGrid('exportdata', 'xls', '数据统计');});
+        $("#print").click(function () {
+            var gridContent = $("#jqxgrid").jqxGrid('exportdata', 'html');
+            var newWindow = window.open('', '', 'width=800, height=500'),
+                    document = newWindow.document.open(),
+                    pageContent =
+                            '<!DOCTYPE html>\n' +
+                            '<html>\n' +
+                            '<head>\n' +
+                            '<meta charset="utf-8" />\n' +
+                            '<title>jQWidgets Grid</title>\n' +
+                            '</head>\n' +
+                            '<body>\n' + gridContent + '\n</body>\n</html>';
+            document.write(pageContent);
+            document.close();
+            newWindow.print();
+        });
     </script>
 @endsection
