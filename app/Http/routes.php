4ab453a6ca089b7df24e9fe3d6a1ad0dee1629e8
Nicholas Stone <Nicholas-Stone@outlook.com>
2016-10-29 20:42:18 +0800

    关闭实验入口

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,7 +16,7 @@ Route::group(['middleware' => 'web'], function () {
      */
     Route::group(['namespace' => 'Frontend'], function () {
         require(__DIR__ . '/Routes/Frontend/Frontend.php');
-        require(__DIR__ . '/Routes/Frontend/Lab.php');
+//        require(__DIR__ . '/Routes/Frontend/Lab.php');
         require(__DIR__ . '/Routes/Frontend/Access.php');
         require(__DIR__ . '/Routes/Frontend/Party.php');
         require(__DIR__ . '/Routes/Frontend/SSOAuth.php');
